996dfd20ae8d50421be32d39f2512ad3f7cd7f55
adilhusain01 <husainadil202@gmail.com>
2024-01-21 23:15:19 +0530

    ...

--- 12-EventLoop.js
+++ 12-EventLoop.js
@@ -1,17 +1,49 @@
 //immediate execution done first then callback functions
+//event loop offloads console.log(result) to file system
 
-const { readFile, writeFile } = require("fs");
+// // #2
+// const { readFile, writeFile } = require("fs");
 
-console.log("starting the first task");
+// console.log("starting the first task");
 
-const first = readFile("text.txt", "utf-8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
-  } else {
-    console.log(result);
-    console.log("completed first task");
-  }
+// const first = readFile("text.txt", "utf-8", (err, result) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   } else {
+//     console.log(result);
+//     console.log("completed first task");
+//   }
+// });
+
+// console.log("starting new task");
+
+// // #2
+// console.log("first");
+
+// setTimeout(() => {
+//   console.log("Hi");
+// }, 0);
+
+// console.log("last");
+
+// #3
+
+// setInterval(() => {
+//   console.log("Hi this is interval");
+// }, 2000);
+
+// console.log("last");
+
+// #4
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("Request event");
+  res.end("Hello world");
 });
 
-console.log("starting new task");
+server.listen(5000, () => {
+  console.log("Server listening on port 5000");
+});
--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const _ = require("lodash");
+const { readFile, writeFile } = require("fs");
+const http = require("http");
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-1;
-console.log("Hello world");
+const server = http.createServer((req, res) => {});
+
+server.listen(5000, () => {
+  console.log("Server listening on port 5000");
+});
