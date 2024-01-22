a2e8566489a21a4962e96027461109cb499da376
adilhusain01 <husainadil202@gmail.com>
2024-01-22 17:21:00 +0530

    ...

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const { readFile, writeFile } = require("fs");
 const http = require("http");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("This is the Home Page");
+  }
+
+  if (req.url === "/about") {
+    res.end("We are 8000 years old");
+  }
+
+  res.end("Error Page");
+});
 
 server.listen(5000, () => {
   console.log("Server listening on port 5000");
