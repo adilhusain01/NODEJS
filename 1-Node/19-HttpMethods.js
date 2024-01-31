a6261e6ac5dbc0ab523bce5700c2e8ed658ad071
adilhusain01 <husainadil202@gmail.com>
2024-01-31 14:00:36 +0530

    ...

---
+++ 1-Node/19-HttpMethods.js
@@ -0,0 +1,15 @@
+//GET
+//GET by id (path params)
+//POST
+//PUT by id (params + send data)
+//DELETE by id (path params)
+
+//Headers &  Body\Payload (Optional, Meta info about the request)
+
+
+
+//Status Codes
+//200 - request succesful
+//400 - request error
+//401 - unauthorized request
+//404 - not found
