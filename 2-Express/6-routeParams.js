e839cd4068b0f2a8226ed07dedbc4eeacd05eed1
adilhusain01 <husainadil202@gmail.com>
2024-02-02 18:26:24 +0530

    ...

--- 2-Express/6-routeParams.js
+++ 2-Express/6-routeParams.js
@@ -1,5 +1,3 @@
-//find method
-
 const express = require("express");
 const { products } = require("./public/data");
 
@@ -9,10 +7,21 @@ app.get('/', (req, res) => {
     res.send(`<h1>Home Page</h1><a href="/api/products">Click here</a>`);
 })
 
-app.get('/api/products/1', (req, res) => {
-    const singleProduct = products.find((product) => product.id==="1");
-    res.json(singleProduct);
+app.get('/api/products/:productID', (req, res) => {
+    const singleProduct = products.find((product) => product.id === req.params.productID);
+
+    if(!singleProduct){
+        res.status(404).json({message:"Not found"});
+    }else{
+        res.json(singleProduct);
+    }
 });
+
+//assuming the complexity
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params);
+    res.send("hello");
+})
 
 
 app.listen(8000, () => {
