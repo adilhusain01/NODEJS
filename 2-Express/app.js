b9a3877edff846ec7a6e82bffaf804e151a85e1d
adilhusain01 <husainadil202@gmail.com>
2024-02-02 18:01:58 +0530

    ...

--- 2-Express/app.js
+++ 2-Express/app.js
@@ -2,23 +2,22 @@
 //server side rendering 
 
 const express = require("express");
-const { products} = require("./public/data");
+const { products } = require("./public/data");
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send(`<h1>Home Page</h1><a href="/api/products">Click here</a>`);
-    res.end();
 })
 
 app.get('/api/products', (req, res) => {
     const newProducts = products.map((product) => {
-        const {id, name, price} = product;
-        return {id, name, price};
+        const {id, name, image} = product;
+        return {id, name, image};
     })
     res.json(newProducts);
-    res.end();
 })
+
 
 app.listen(8000, () => {
     console.log("Server running on port 8000");
