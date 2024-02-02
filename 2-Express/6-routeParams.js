6b46c9d083b59ed7fa2a0ecd1f86d7eb7a90266c
adilhusain01 <husainadil202@gmail.com>
2024-02-02 18:12:05 +0530

    ...

--- 2-Express/6-routeParams.js
+++ 2-Express/6-routeParams.js
@@ -1,7 +1,4 @@
-//we send application json data
-//server side rendering
-
-//map method
+//find method
 
 const express = require("express");
 const { products } = require("./public/data");
@@ -12,13 +9,10 @@ app.get('/', (req, res) => {
     res.send(`<h1>Home Page</h1><a href="/api/products">Click here</a>`);
 })
 
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const {id, name, image} = product;
-        return {id, name, image};
-    })
-    res.json(newProducts);
-})
+app.get('/api/products/1', (req, res) => {
+    const singleProduct = products.find((product) => product.id==="1");
+    res.json(singleProduct);
+});
 
 
 app.listen(8000, () => {
