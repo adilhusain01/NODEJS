b9864fd483fa8bdf58571fadd692a0a3012e8eb2
adilhusain01 <husainadil202@gmail.com>
2024-02-05 23:40:42 +0530

    configuring post method

--- 2-Express/11-postSetup.js
+++ 2-Express/11-postSetup.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 
 const app = express();
-const { people } = require("./public/daata");
+const { people } = require("./public/data");
 
 //static assets
 app.use(express.static('./public/methods-public'));
