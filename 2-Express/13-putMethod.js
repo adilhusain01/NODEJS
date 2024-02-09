9bffa47e29f538c088dc6f250ae416be6be774aa
adilhusain01 <husainadil202@gmail.com>
2024-02-09 17:12:24 +0530

    fetching the data and then updating it according to the request

---
+++ 2-Express/13-putMethod.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const { people } = require('./public/data');
+
+const app = express();
+
+ app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send({success: true});
+})
+
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params;
+    const { name }  = req.body;
+
+    const person = people.find((person) => person.id === Number(id));
+    
+    if(!person){
+        return res.status(404).json({success: false, msg: `No person with ID ${id}`});
+    }
+
+    const newPerson = people.map((person) => {
+        if(person.id === Number(id)){
+            person.name = name;
+        }
+
+        return person; 
+    })
+ 
+    res.status(200).json({success:true, data:newPerson});
+
+})
+
+app.listen(8000, () => {
+    console.log("Server running on port 8000");
+})
